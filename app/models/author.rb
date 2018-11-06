6ad2fd28755aa09caa4fa8b975f8f834e621394d
HannaStrezh <hannastrezh@gmail.com>
2018-11-06 11:07:00 -0500

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,28 @@
 class Author
-  attr_accessor :name
-
+  attr_reader :name
+  attr_writer :title
+  @@all = []
 
   def initialize(name)
     @name = name
-  
-  end
+    @title = title
+    @@all << self
+    end
 
+    def self.all
+    @@all
+    end
+
+def author(give_article)
+  Article.all.find do |article|
+  article.author == given_article
+end
+
+def magazine(given_article)
+  Article.all.find do |mag|
+  mag.magazine == given_article
+end
 
 end
+
+end#end of an Author class
