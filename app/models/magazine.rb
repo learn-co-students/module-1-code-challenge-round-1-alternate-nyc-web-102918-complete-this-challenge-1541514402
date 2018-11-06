32aadfd081aea31f23d210f9e17edc8ed83c21aa
no0ro <nrolwes@gmail.com>
2018-11-06 11:41:50 -0500

    done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,50 @@
 class Magazine
-  attr_accessor :name, :category
+  attr_accessor :name, :category, :titles #both strings, accessor
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
+    @titles = titles
+  end
 
+  def self.all
+    @@all
   end
 
 
+#!!!
+  def self.find_by_name(string_mag_name)
+    string_mag_name = string_mag_name.to_sym
+    self.all.find do |mag|
+      mag.name == string_mag_name
+    end
+  end
+  #   + Given a string of magazine's name, this method returns the first magazine object that matches
+
+#What question
+  def article_titles
+
+  end
+  #   + Returns an array of the titles of all articles written for that magazine
+
 end
+
+
+
+# ### Magazine
+# --  A magazine is initialized with a name as a string and a category as a string.
+#  --  The name of the magazine and the category it falls under **can be** changed after being initialized.
+#
+# + `Magazine#name`
+#   + Returns the name of this magazine
+# + `Magazine#category`
+#   + Returns the category of this magazine
+# + `Magazine.all`
+#   + Returns an array of all Magazine instances
+# + `Magazine.find_by_name(name)`
+#   + Given a string of magazine's name, this method returns the first magazine object that matches
+# + `Magazine#article_titles`
+#   + Returns an array of the titles of all articles written for that magazine
--- tools/console.rb
+++ tools/console.rb
@@ -5,9 +5,21 @@ def reload
 end
 
 
-###  WRITE YOUR TEST CODE HERE ###
+a1 = Author.new("mary")
+a2 = Author.new("Brad")
+a3 = Author.new("Faith")
+a4 = Author.new("Sam")
 
 
+m1 = Magazine.new("Vogue", "Fashion")
+m2 = Magazine.new("People", "Live")
+m3 = Magazine.new("US", "Gossip")
+m4 = Magazine.new("SI", "Sports")
+
+art1 = Article.new(a1,m2)
+art2 = Article.new(a2,m2)
+art3 = Article.new(a4,m3)
+art4 = Article.new(a1,m1)
 
 
 
